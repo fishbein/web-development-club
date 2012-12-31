b2d75c6b7c6b3b9671f2eb4423e2bade1dbcb175
Philip Fishbein <phil@fishbe.in>
2012-12-30 21:33:20 -0500

    Added Loops and Arrays

---
+++ loops-and-arrays.php
@@ -0,0 +1,39 @@
+<?php
+	$name = array("Phil", "Jacob", "Dan", "Paul", "Robin");
+	$age = array(16, 17, 18, 19);
+
+	$number = $_POST['number'];
+	$rand1 = $_POST['rand1'];
+	$rand2 = $_POST['rand2'];
+	$operation = $_POST['operation'];
+
+	//Foreach Loops
+	foreach ($name as $value => $name) {
+		echo "<li>".$name."</li>";
+	}
+	foreach ($age as $value => $turkey) {
+		echo "<li>".$turkey."</li>";
+	}
+
+	//If Loops
+	if($operation=="+"){
+		$equation = $rand1+$rand2;
+	}
+	else if($operation=="-"){
+		$equation = $rand1-$rand2;
+	}
+	else if($operation=="*"){
+		$equation = $rand1*$rand2;
+	}
+	else if($operation=="/"){
+		$equation = $rand1/$rand2;
+	}
+	
+	echo $equation;
+	if($equation==$number){
+		echo "Correct";
+	}
+	else{
+		echo "Incorrect";
+	}
+?>
